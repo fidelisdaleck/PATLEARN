d5c1d9270ddcd059b47ca383b86616e8216e9cbd
fidelisdaleck <fidelisdaleck386@gmail.com>
2026-07-10 12:33:21 +0100

    Description des modifications

---
+++ sauvegarde-results/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Suspense } from "react";
+import ResultsContent from "./ResultsContent";
+
+export default function ResultsPage() {
+  return (
+    <Suspense fallback={<div className="flex justify-center items-center min-h-screen">Chargement...</div>}>
+      <ResultsContent />
+    </Suspense>
+  );
+}
+
+
+
+
